bed9eac898aea1ae1547907c74aa1d2863817c17
chinmoybuzz <chinmoybuzz@gmail.com>
2025-06-01 04:28:28 +0530

    updateed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const apiRoute = require("./routes/api.v1.route");
-const connectDb = require("./Db/dbConnect");
+const connectDb = require("./db/dbConnect");
 
 const start = async () => {
   app.use(cors());
