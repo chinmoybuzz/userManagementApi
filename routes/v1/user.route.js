d8104573a0035f17877a221f9cacb04c307ac357
chinmoybuzz <145897023+chinmoybuzz@users.noreply.github.com>
2025-06-01 17:56:11 +0530

    Update user.route.js

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../../controller/user.controller");
 const { authMiddleware, roleChecker } = require("../../middleware/authMiddleware");
-router.route("/list").get(authMiddleware, UserController.list);
+router.route("/list").get( UserController.list);
 router.route("/tree-view").get(authMiddleware, UserController.tree);
 router.route("/update-parent/parent").patch(authMiddleware, UserController.updateParentId);
 module.exports = router;
